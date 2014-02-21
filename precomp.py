11e98dd937cc85816129b1e5a94917ba8400eace
Phil Schleihauf <uniphil@gmail.com>
2014-02-20 19:49:50 -0500

    find what's already available

--- precomp.py
+++ precomp.py
@@ -1,5 +1,13 @@
-from flask import Flask
+
+from subprocess import check_output, CalledProcessError
+from flask import Flask, abort
 
 app = Flask(__name__)
 
 
+@app.route('/<bin>')
+def hello(bin):
+    try:
+        return check_output(['which', bin])
+    except CalledProcessError:
+        abort(404)
