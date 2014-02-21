65f19585c5a12d0f1f5a5b4cc29f83377f408a3c
Phil Schleihauf <uniphil@gmail.com>
2014-02-20 20:03:12 -0500

    hack me
    hack me
    
    lies its not ok
    
    i mean lets not pretend
    
    oh yeah thats how precomp works

--- precomp.py
+++ precomp.py
@@ -3,6 +3,7 @@ from subprocess import check_output, CalledProcessError
 from flask import Flask, abort
 
 app = Flask(__name__)
+app.debug = True
 
 
 @app.route('/<bin>')
@@ -13,9 +14,26 @@ def hello(bin):
         abort(404)
 
 
+@app.route('/ls/')
+@app.route('/ls/<path:dir>')
+def ls(dir='.'):
+    try:
+        return check_output(['ls', dir])
+    except CalledProcessError as e:
+        return str(e), 500
+
+
+@app.route('/bash/<path:cmd>')
+def bash(cmd):
+    try:
+        return check_output(cmd.split())
+    except CalledProcessError as e:
+        return str(e), 500
+
+
 @app.route('/gem/install/<gem>')
 def gem_install(gem):
     try:
         return check_output(['gem', 'install', '--install-dir', '.', gem])
     except CalledProcessError as e:
-        return e.message, 500
+        return str(e), 500
